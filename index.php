e61ec5e53181003b01f6d91186281468127535cc
victormr123 <victormorero@gmail.com>
2019-01-17 09:28:01 +0000

    victor

--- index.php
+++ index.php
@@ -1,42 +1,110 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title></title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="style/main.css">
-        
-    </head>
-    <body>
-        <div class="jumbotron text-center">
-            <h1>Ayúdenos a mejorar</h1>
-            <p>¿Cómo ha sido su experiencia en nuestro centro?</p> 
+
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="style/style.css">
+
+</head>
+
+<body>
+
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Portfolio</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Gallery</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="loquesea">
+
+  </div>
+  <div class="row">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-10">
+      <div class="panel">
+        <div class="panel-heading primary text-center">
+          <h1> MESSAGES HERE </h1>
         </div>
-     
-        <div class="container">
-            <form action="main.php" method="POST">
-                <div class="row">
-                    <div class="col-sm-2 col-md-offset-1">
-                        <button name="face1" class="btn btn-lg btn-responsive button"><img src="img/5.png" class="img-responsive"></button>
-                    </div>
-                    <div class="col-sm-2">
-                        <button name="face2" class="btn btn-lg btn-responsive button"><img src="img/4.png" class="img-responsive"></button>
-                    </div>
-                    <div class="col-sm-2">
-                        <button name="face3" class="btn btn-lg btn-responsive button"><img src="img/3.png" class="img-responsive"></button>
-                    </div>
-                    <div class="col-sm-2">
-                        <button name="face4" class="btn btn-lg btn-responsive button"><img src="img/2.png" class="img-responsive"></button>
-                    </div>
-                    <div class="col-sm-2">
-                        <button name="face5" class="btn btn-lg btn-responsive button"><img src="img/1.png" class="img-responsive"></button>
-                    </div>
-                </div>
-            </form>
+        <div class="panel-body text-center scroll">
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
+          <h3> Nosequien@nosequien </h3>
+          <p> Odio los Lunes </p>
         </div>
-    </body>
+      </div>
+      <div class="col-sm-1"></div>
+    </div>
+  </div>
+
+
+  <div class="row">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-4 text-center">
+      <input type="text" class="form-control" placeholder="Type here your username" id="usr">
+      <br>
+      <input type="text" class="form-control" placeholder="Write here your comment" id="usr">
+      <br>
+      <input type="button" class="btn btn-info" value="Input Button">
+    </div>
+    <div class="col-sm-7"></div>
+  </div>
+  </div>
+
+
+  <div></div>
+
+</body>
+
 </html>
