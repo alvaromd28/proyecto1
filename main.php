9b9304d10a288c6c4f9acc4f0007d035de3db4a5
TomasSB <tomiteror96@gmail.com>
2019-01-17 09:09:43 +0000

    first commit t

--- main.php
+++ main.php
@@ -1,28 +1,9 @@
 <?php
     require_once('util/db_manager.php');
-
-     if( isset($_POST['face1'])){
-         echo "Pésimo";
-         insertRating('1');
-     }
-
-     if( isset($_POST['face2'])){
-        echo "Mal";
-        insertRating('2');
+    if (isset($_POST['submmit'])){
+        $name = $_POST['name'];
+        $text = $_POST['text'];
+        insert($name,$text);
     }
-
-    if( isset($_POST['face3'])){
-        echo "Regular";
-        insertRating('3');
-    }
-
-    if( isset($_POST['face4'])){
-        echo "Está bien";
-        insertRating('4');
-    }
-
-    if( isset($_POST['face5'])){
-        echo "Genial";
-        insertRating('5');
-    }
+    get_msg();
 ?>
